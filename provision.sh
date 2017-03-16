47207fa8ba534d57e468788110bf880721f91740
Evheniy Bystrov <evheniy.bystrov@gmail.com>
2017-03-16 08:41:20 +0000

    redis + mysql

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 yes | sudo apt-get upgrade
 yes | sudo apt-get dist-upgrade
-yes | sudo apt-get install --reinstall locales git curl language-pack-en-base nginx
+yes | sudo apt-get install --reinstall locales git curl language-pack-en-base nginx apache2-utils redis-server make mysql-server mysql-client
 sudo sh -c "sudo echo 'LC_ALL=\"en_US.UTF-8\"' >> /etc/environment"
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
